0614e2e034119699782a40a2b8009d6757a6e505
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-01-09 20:33:45 +0400

    Add new SVG icons for arrow up, Facebook, and in
    - Added arrowup.svg for upward navigation.
    - Added fb.svg for Facebook icon.
    - Added in.svg for an input-related icon.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
-import Footer from "./components/Footer";
 
 import WhoWeAre from "./pages/WhoWeAre";
 import WhatWeDo from "./pages/WhatWeDo";
