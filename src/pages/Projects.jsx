1f74b7b0ed3411bf02ddeebee39ec15c27ae6824
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-01-13 22:31:23 +0400

    Enhance Header component with dropdown icon; implement Projects page layout and filtering; adjust footer image styling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 const Projects = () => {
-  return <div></div>;
+  const filterItems = [
+    { type: "All" },
+    { type: "International" },
+    { type: "Azerbaijan" },
+  ];
+  return (
+    <div className="projectsPage">
+      <div className="container">
+        <div className="breadcrumb">
+          <a href="#">Glensol</a> <a href="#">Our projects</a>
+        </div>
+        <div className="projectsMain">
+          <div className="pageTitle">
+            <span>Our projects</span>
+            <h1>Projects</h1>
+          </div>
+          <div className="filter">
+            <ul>
+              {filterItems.map((item, index) => (
+                <li key={index}>
+                  <button className="filterItemActive">{item.type}</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
