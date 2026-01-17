61ae0dea38a837d9b42f92de527dd79894764906
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-01-17 22:51:59 +0400

    Refactor Contact page layout and styling; update class names for consistency and improve form structure - BEM

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,41 +10,41 @@ import ArrowRight from "../assets/icons/arrowright.svg";
 
 const Contact = () => {
   return (
-    <div className="contactPage">
+    <div className="contact">
       <div className="container">
-        <div className="breadcrumb">
-          <NavLink to="/">Glensol</NavLink> /{" "}
+        <div className="contact__breadcrumb">
+          <NavLink to="/">Glensol</NavLink>
+          &nbsp; / &nbsp;
           <NavLink to="/contact">Əlaqə</NavLink>
         </div>
       </div>
 
-      <div className="contactPageMain">
-        <div className="contactPageTop">
+      <div className="contact__main">
+        <div className="contact__top">
           <div className="container">
-            <div className="pageTitle">
+            <div className="contact__title">
               <span>Əlaqə</span>
               <h1>Bizimlə əlaqə</h1>
               <p>
                 Xidmətlərimiz barədə daha ətraflı öyrənmək və ya sual vermək
-                istəyirsiniz? Bizimlə əlaqə saxlayın, qısa zamanda sizə cavab
-                verəcəyik.
+                istəyirsiniz? Bizimlə əlaqə saxlayın.
               </p>
             </div>
           </div>
 
           <div className="container">
-            <div className="contactItem">
+            <div className="contact__item">
               <img src={PhoneLogo} alt="Phone" />
               <a href="tel:+994123100819">+99412 310 08 19</a>
-              <a href="mailto:+994123100820">+99412 310 08 20</a>
+              <a href="tel:+994123100820">+99412 310 08 20</a>
             </div>
 
-            <div className="contactItem">
+            <div className="contact__item">
               <img src={EmailLogo} alt="Email" />
               <a href="mailto:info@glensol.az">info@glensol.az</a>
             </div>
 
-            <div className="contactAddress">
+            <div className="contact__item contact__item--address">
               <img src={LocationLogo} alt="Location" />
               <a
                 href="https://www.google.com/maps?q=40.38071071416792,49.87833578160276"
@@ -55,9 +55,10 @@ const Contact = () => {
               </a>
             </div>
 
-            <div className="contactItemSocials">
+            <div className="contact__social">
               <p>Bizi izləyin</p>
-              <div className="item">
+
+              <div className="contact__social-icons">
                 <a
                   href="https://www.facebook.com/Global.Energy.Solutions.GLENSOL/"
                   target="_blank"
@@ -79,70 +80,45 @@ const Contact = () => {
         </div>
       </div>
 
-      <div className="contactPageBottom">
+      <div className="contact__bottom">
         <div className="container">
-          <div className="contactForm">
-            <div className="contactFormContainer">
-              <div className="contactFormLeft">
-                <div className="contactFormTitle">
-                  <h3>Sualınız var?</h3>
-                </div>
-
-                <form>
-                  <div className="formInputs">
-                    <div className="customInputContainer">
-                      <label htmlFor="fullname">Ad Soyad</label>
-                      <input
-                        id="fullname"
-                        className="customInput"
-                        name="fullName"
-                        type="text"
-                      />
-                    </div>
-
-                    <div className="customInputContainer">
-                      <label htmlFor="email">E-poçt</label>
-                      <input
-                        id="email"
-                        className="customInput"
-                        name="email"
-                        type="email"
-                      />
-                    </div>
-
-                    <div className="customInputContainer mobile">
-                      <label htmlFor="mobileNumber">Mobil nömrə</label>
-                      <div className="mobileInput">
-                        <input
-                          id="mobileNumber"
-                          autoComplete="tel"
-                          placeholder="Enter phone number"
-                          className="phoneInput"
-                          type="tel"
-                          defaultValue="+994"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="customInputContainer">
-                      <label htmlFor="message">Mesajınız</label>
-                      <textarea
-                        id="message"
-                        className="customInput"
-                        name="message"
-                      ></textarea>
-                    </div>
-                  </div>
-
-                  <div className="formButton">
-                    <button className="button-submit" type="submit">
-                      Göndər
-                      <img src={ArrowRight} alt="Arrow Right" />
-                    </button>
-                  </div>
-                </form>
-              </div>
+          <div className="contact__form">
+            <div className="contact__form-title">
+              <h3>Sualınız var?</h3>
             </div>
+
+            <form className="contact__fields">
+              <div className="contact__field">
+                <label htmlFor="fullname">Ad Soyad</label>
+                <input id="fullname" className="contact__input" />
+              </div>
+
+              <div className="contact__field">
+                <label htmlFor="email">E-poçt</label>
+                <input id="email" className="contact__input" />
+              </div>
+
+              <div className="contact__field contact__field--phone">
+                <label htmlFor="mobile">Mobil nömrə</label>
+                <input
+                  id="mobile"
+                  className="contact__phone-input"
+                  defaultValue="+994"
+                />
+              </div>
+
+              <div className="contact__field">
+                <label htmlFor="message">Mesajınız</label>
+                <textarea id="message" className="contact__input"></textarea>
+              </div>
+
+              <div className="contact__actions">
+                <button className="contact__submit">
+                  Göndər
+                  <img src={ArrowRight} alt="Arrow" />
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
