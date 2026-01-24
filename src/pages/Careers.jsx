421fdf3c96ad656bed995ae7e42b511484a4d17e
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-01-24 22:49:29 +0400

    Refactor code structure for improved readability and maintainability

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import CareersImage from "../assets/Images/careers.jpg";
 const Careers = () => {
-  return <div></div>;
+  return (
+    <div className="careersPage">
+      <div className="pageBanner">
+        <div className="bannerImage">
+          <img src={CareersImage} alt="Career Banner Image" />
+        </div>
+        <div className="container">
+          <div className="pageBannerTitle">
+            <span>Work with us</span>
+            <h3>Careers</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Careers;
