a7d715473bbd5154fa559f5331864cbb53f858ab
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-01-25 01:03:24 +0400

    Refactor code structure for improved readability and maintainability

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 
 import CareersImage from "../assets/Images/careers.jpg";
+import BottomCareers from "../assets/Images/bottomcareers.png";
 const Careers = () => {
   return (
     <div className="careersPage">
-      <div className="pageBanner">
-        <div className="bannerImage">
-          <img src={CareersImage} alt="Career Banner Image" />
+      <div className="pageMain">
+        <div className="pageBanner">
+          <div className="bannerImage">
+            <img src={CareersImage} alt="Career Banner Image" />
+          </div>
+          <div className="container">
+            <div className="pageBannerTitle">
+              <span>Work with our company</span>
+              <h1>Careers</h1>
+            </div>
+          </div>
         </div>
         <div className="container">
-          <div className="pageBannerTitle">
-            <span>Work with us</span>
-            <h3>Careers</h3>
+          <div className="pageTopTitle">
+            <span>Career</span>
+            <h3>Work with our company</h3>
+          </div>
+          <div className="bottomPage">
+            <div className="bottomImage">
+              <img src={BottomCareers} alt="Bottom Image Careers" />
+            </div>
+            <div className="bottomText">
+              <h3>Drive the Future of Energy Services with Us</h3>
+              <p>
+                At Glensol, we believe our people are our most valuable asset.
+                As a leading provider of integrated oilfield services, we are
+                committed to attracting, developing, and retaining top talent
+                who are passionate about making an impact in the energy sector.
+              </p>
+            </div>
           </div>
         </div>
       </div>
