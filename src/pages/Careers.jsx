54b7cad49137ae7d390861c8ec8836e34449cec9
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-01-25 19:59:19 +0400

    Enhance Careers page layout with new bottom section and responsive styles

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import CareersImage from "../assets/Images/careers.jpg";
 import BottomCareers from "../assets/Images/bottomcareers.png";
+import ArrowRight from "../assets/icons/arrowright.svg";
 const Careers = () => {
   return (
     <div className="careersPage">
@@ -22,7 +23,36 @@ const Careers = () => {
             <span>Career</span>
             <h3>Work with our company</h3>
           </div>
-          <div className="bottomPage"></div>
+          <div className="bottomPage">
+            <div className="bottomPageBanner">
+              <img src={BottomCareers} alt="Career" />
+            </div>
+            <div className="bottomRight">
+              <h3>Drive the Future of Energy Services with Us</h3>
+              <p>
+                At Glensol, we believe our people are our most valuable asset.
+                As a leading provider of integrated oilfield services, we are
+                committed to attracting, developing, and retaining top talent
+                who are passionate about making an impact in the energy sector.
+              </p>
+              <a href="#">
+                Why Join Glensol
+                <img src={ArrowRight} alt="ArrowRight" />
+              </a>
+            </div>
+          </div>
+          <div className="centerText">
+            <div className="text">
+              <h3>Internships & Graduate Programs</h3>
+              <p>
+                Glensol offers internship and graduate opportunities for
+                students and young professionals seeking hands-on experience in
+                the energy services sector. You’ll gain exposure to real-world
+                projects, receive mentoring from industry experts, and build the
+                foundation for a successful career.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
