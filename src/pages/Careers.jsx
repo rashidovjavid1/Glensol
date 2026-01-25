b9a133b71d5a0a967d82f8d93d3d880a4b295606
rashidovjavid1 <rashidovjavid1@gmail.com>
2026-01-26 01:31:40 +0400

    Refactor code structure for improved readability and maintainability

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
-import CareersImage from "../assets/Images/careers.jpg";
-import BottomCareers from "../assets/Images/bottomcareers.png";
-import ArrowRight from "../assets/icons/arrowright.svg";
+import CareersImage from "../assets/Images/careers.png";
+import BottomCareers from "../assets/Images/bottomCareers.png";
+import ArrowRight from "../assets/icons/arrowRight.svg";
 const Careers = () => {
+  const openVacancies = [
+    {
+      role: "Sales Manager",
+      date: "9 September 2025",
+      employment: "Full-time/Part-time/Contract",
+      location: "Baku, Azerbaijan",
+    },
+    {
+      role: "Product Manager",
+      date: "9 September 2025",
+      employment: "Full-time/Part-time/Contract",
+      location: "Baku, Azerbaijan",
+    },
+    {
+      role: "Supply Chain Manager",
+      date: "9 September 2025",
+      employment: "Full-time/Part-time/Contract",
+      location: "Baku, Azerbaijan",
+    },
+  ];
   return (
     <div className="careersPage">
       <div className="pageMain">
@@ -18,30 +38,32 @@ const Careers = () => {
             </div>
           </div>
         </div>
-        <div className="container">
-          <div className="pageTopTitle">
+        <div className="pageTopTitle">
+          <div className="container">
             <span>Career</span>
             <h3>Work with our company</h3>
           </div>
-          <div className="bottomPage">
-            <div className="bottomPageBanner">
-              <img src={BottomCareers} alt="Career" />
-            </div>
-            <div className="bottomRight">
-              <h3>Drive the Future of Energy Services with Us</h3>
-              <p>
-                At Glensol, we believe our people are our most valuable asset.
-                As a leading provider of integrated oilfield services, we are
-                committed to attracting, developing, and retaining top talent
-                who are passionate about making an impact in the energy sector.
-              </p>
-              <a href="#">
-                Why Join Glensol
-                <img src={ArrowRight} alt="ArrowRight" />
-              </a>
-            </div>
+        </div>
+        <div className="bottomPage">
+          <div className="bottomPageBanner">
+            <img src={BottomCareers} alt="Career" />
           </div>
-          <div className="centerText">
+          <div className="bottomRight">
+            <h3>Drive the Future of Energy Services with Us</h3>
+            <p>
+              At Glensol, we believe our people are our most valuable asset. As
+              a leading provider of integrated oilfield services, we are
+              committed to attracting, developing, and retaining top talent who
+              are passionate about making an impact in the energy sector.
+            </p>
+            <a href="#">
+              Why Join Glensol
+              <img src={ArrowRight} alt="ArrowRight" />
+            </a>
+          </div>
+        </div>
+        <div className="centerText">
+          <div className="container">
             <div className="text">
               <h3>Internships & Graduate Programs</h3>
               <p>
@@ -51,6 +73,13 @@ const Careers = () => {
                 projects, receive mentoring from industry experts, and build the
                 foundation for a successful career.
               </p>
+            </div>
+          </div>
+        </div>
+        <div className="vacanciesBanner">
+          <div className="container">
+            <div className="vacanciesBannerTop">
+              <h3>Open Vacancies</h3>
             </div>
           </div>
         </div>
